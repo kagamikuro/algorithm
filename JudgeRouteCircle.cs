aad5ca9e7d9d0ccbb74e747097ef33ab64601021
kagamikuro <l>
2018-02-10 19:54:41 +0800

    upload 657. Judge Route Circle

---
+++ JudgeRouteCircle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class JudgeRouteCircle
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int x = 0;
+            int y = 0;
+            for(int n = 0; n < moves.Length; n++)
+            {
+                if (moves[n] == 'U')
+                {
+                    y++;
+                    continue;
+                }
+                if (moves[n] == 'D')
+                {
+                    y--;
+                    continue;
+                }
+
+                if (moves[n] == 'R')
+                {
+                    x++;
+                    continue;
+                }
+                if (moves[n] == 'L')
+                    x--;               
+            }
+            return x == 0 && y == 0;
+        }
+    }
+}
