e0ddd3ebe42fee211efb1637f5a8652fe6583ee4
kagamikuro <l>
2018-02-10 19:35:55 +0800

    update 771. Jewels and Stones

---
+++ JewelsAndStones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class JewelsAndStones
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            if (J.Length == 0 || S.Length == 0)
+                return 0;
+            int nums = 0;
+            for(int n = 0; n < S.Length; n++)
+            {
+                for(int m = 0; m < J.Length; m++)
+                {
+                    if (S[n] == J[m])
+                        nums++;
+                }
+            }
+            return nums;
+        }
+    }
+    
+}
